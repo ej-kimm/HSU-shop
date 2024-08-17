58e963c81822b1222b17a481a3d84854c8757f5d
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-08-18 02:29:35 +0900

    ✨ Implement ProductDetail page with option selector and add-to-cart button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function ProductCard({
+  product,
   product: { id, image, title, category, price },
 }) {
+  const navigate = useNavigate()
+
   return (
-    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105">
+    <li
+      onClick={() => {
+        navigate(`products/${id}`, { state: { product } })
+      }}
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+    >
       <img className="w-full h-96 object-cover" src={image} alt={title} />
       <div className="m-2">
         <div className="text-lg flex justify-between items-center">
